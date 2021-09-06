2059623bfae1dca2bc1a6cb84e60f21696535178
ahmadfatihin <ahmadfatihin69@gmail.com>
2021-09-06 10:03:44 +0700

    master

---
+++ database/migrations/2021_09_06_013540_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('harga');
+            $table->boolean('is_ready')->default(true);
+            $table->string('jenis');
+            $table->float('berat')->default(0.33);
+            $table->string('gambar');
+            $table->integer('kategori_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
