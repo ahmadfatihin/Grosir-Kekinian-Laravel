67afdb7f29472f87a941e42445c3ead2fd250253
ahmadfatihin <ahmadfatihin69@gmail.com>
2021-09-07 10:30:29 +0700

    create slider and category

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\kategori;
+use App\Product;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.home');
+        return view('livewire.home', [
+            'products' => Product::take(4)->get(),
+            'kategoris' => kategori::all()
+        ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <h1>Test</h1>
+<div class="container">
+
+    {{-- BANNER --}}
+    <div class="banner">
+        <img src="{{ url('Assets/slider/Banner.png') }}" alt="">
+    </div>
+
+    {{-- PILIH KATEGORI --}}
+    <section class="pilih-kategori mt-5">
+        <h3><strong>Pilih Kategori</strong></h3>
+        <div class="row mt-3">
+            @foreach ($kategoris as $kategori)
+            <div class="col-sm-3">
+                <div class="card shadow">
+                    <div class="card-body text-center">
+                      <img src="{{ url('Assets/kategori') }}/{{ $kategori->gambar }}" alt="" class="img-fluid"><br>
+                      <h5 class="text-center"><strong>{{ $kategori->nama }}</strong></h5>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+    </section>
 </div>
