e59b6ebeee09dd71fe5d03f6ee4f56b6a6691956
ahmadfatihin <ahmadfatihin69@gmail.com>
2021-09-07 11:32:17 +0700

    Add category in home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -5,16 +5,50 @@
         <img src="{{ url('Assets/slider/Banner.png') }}" alt="">
     </div>
 
-    {{-- PILIH KATEGORI --}}
+    {{-- KATEGORI --}}
     <section class="pilih-kategori mt-5">
         <h3><strong>Pilih Kategori</strong></h3>
-        <div class="row mt-3">
+        <div class="row mt-3 text-center">
             @foreach ($kategoris as $kategori)
-            <div class="col-sm-3">
+            <div class="col-xs-4 col-sm-3 col-md-3">
+                <div class="card shadow">
+                    <div class="card-body text-center">
+                      <img src="{{ url('Assets/kategori') }}/{{ $kategori->gambar }}" alt="" class="img-fluid"><br>
+                      <h5 class="card-subtitle text-center"><strong>{{ $kategori->nama }}</strong></h5>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+    </section>
+    <section class="pilih-kategori mt-5">
+        <h3><strong>Pilih Kategori</strong></h3>
+        <div class="row mt-3 text-center">
+            @foreach ($kategoris as $kategori)
+            <div class="col-xs-4 col-sm-3 col-md-3">
                 <div class="card shadow">
                     <div class="card-body text-center">
                       <img src="{{ url('Assets/kategori') }}/{{ $kategori->gambar }}" alt="" class="img-fluid"><br>
                       <h5 class="text-center"><strong>{{ $kategori->nama }}</strong></h5>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+    </section>
+
+    {{-- PRODUCT--}}
+    <section class="pilih-kategori mt-5">
+        <h3><strong>Product</strong></h3>
+        <div class="row mt-3">
+            @foreach ($products as $product)
+            <div class="col-xs-6">
+                <div class="card shadow">
+                    <div class="card-body text-center">
+                      <img src="{{ url('Assets/kategori') }}/{{ $product->gambar }}" alt="" class="img-fluid"><br>
+                      <h5 class="card-subtitle text-center"><strong>{{ $product->nama }}</strong></h5>
                     </div>
                 </div>
             </div>
