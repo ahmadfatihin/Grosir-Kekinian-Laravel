dd5b9f098ff4a6c2bcbc51c40f0fc8ce2cf87817
ahmadfatihin <ahmadfatihin69@gmail.com>
2021-09-07 15:48:03 +0700

    add assets and footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <hr>
+    <div class="container">
+        <div class="row mb-3">
+                <div class="col text-center">
+                Copyright @Fatih 2021
+            </div>
+        </div>
+    </div>
+   
+
+</footer>
