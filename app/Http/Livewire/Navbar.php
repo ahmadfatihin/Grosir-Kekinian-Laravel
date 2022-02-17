c702e9296998211cd63cfedbecbcd8addf46c49f
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-02-17 23:24:43 +0700

    Add Product

--- app/Http/Livewire/Navbar.php
+++ app/Http/Livewire/Navbar.php
@@ -12,33 +12,33 @@ class Navbar extends Component
 {
     public $jumlah = 0;
 
-protected $listeners = [
-    'masukKeranjang' => 'updateKeranjang'
-];
+    protected $listeners = [
+        'masukKeranjang' => 'updateKeranjang'
+    ];
 
-public function updateKeranjang()
-{
-    if(Auth::user()) {
-        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-        if($pesanan) {
-            $this->jumlah = PesananDetail::where('pesanan_id', $pesanan->id)->count();
-        }else {
-            $this->jumlah = 0;
+    public function updateKeranjang()
+    {
+        if(Auth::user()) {
+            $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+            if($pesanan) {
+                $this->jumlah = PesananDetail::where('pesanan_id', $pesanan->id)->count();
+            }else {
+                $this->jumlah = 0;
+            }
         }
     }
-}
 
-public function mount()
-{
-    if(Auth::user()) {
-        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
-        if($pesanan) {
-            $this->jumlah = PesananDetail::where('pesanan_id', $pesanan->id)->count();
-        }else {
-            $this->jumlah = 0;
+    public function mount()
+    {
+        if(Auth::user()) {
+            $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+            if($pesanan) {
+                $this->jumlah = PesananDetail::where('pesanan_id', $pesanan->id)->count();
+            }else {
+                $this->jumlah = 0;
+            }
         }
-    }
-   
+    
 }
     public function render()
     {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    {{-- Fatih Gunakan Async Defer !!--}}
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/owl.carousel.js') }}" async></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
